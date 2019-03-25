6fc07873988287a870de5f6fdce9278a4d1b9926
Marisa Wadsworth <10002444@student.toiohomai.ac.nz>
2019-03-25 16:13:22 +1300

    Task 1 Done

--- Dictionary1/Dictionary1/Program.cs
+++ Dictionary1/Dictionary1/Program.cs
@@ -4,15 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dictionary1
+namespace Dictionary
 {
     class Program
     {
         static void Main(string[] args)
         {
+            const int count = 5;
             Dictionary<int, string> dict = new Dictionary<int, string>();
+            for(int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Please enter a name.");
+                string name = Console.ReadLine();
+                dict.Add(i, name);
+            }
 
-            Console.WriteLine("");
+            foreach (KeyValuePair<int, string> x in dict)
+                Console.WriteLine($"Key: {x.Key} \tValue: {x.Value}");
         }
     }
 }
