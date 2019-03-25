ad1449b495ba5b7e7e1801e013b1e1037dfbd0d3
Marisa Wadsworth <10002444@student.toiohomai.ac.nz>
2019-03-25 16:04:52 +1300

    First commit

---
+++ Dictionary1/Dictionary1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+
+            Console.WriteLine("");
+        }
+    }
+}
